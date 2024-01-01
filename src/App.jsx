daa8a06170dea7392c2630dcf6f9b2308a763c38
victuseze <106381679+victuseze@users.noreply.github.com>
2024-01-01 20:58:36 +0000

    Beginning

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Beginning Of A New World! </h1>
+    </>
+  )
+}
+
+export default App
